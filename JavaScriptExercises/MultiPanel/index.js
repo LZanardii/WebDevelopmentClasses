a11ee0b7b9cf9ac87fddd5d5d66ccd6925fcd5e5
Leonardo Zanardi <leonardo.szanardi@gmail.com>
2022-08-19 19:05:34 -0300

    rename packages + MultiPanel

---
+++ JavaScriptExercises/MultiPanel/index.js
@@ -0,0 +1,21 @@
+var firstButton = document.getElementById("first");
+var secondButton = document.getElementById("second");
+var thurdButton = document.getElementById("third");
+
+var card = document.getElementById("card");
+
+firstButton.addEventListener('click', function onClick(event) {
+    card.style.backgroundColor = 'green';
+    card.innerHTML=""
+})
+
+secondButton.addEventListener('click', function onClick(event) {
+    card.style.backgroundColor = 'blue';
+    card.innerHTML=""
+    firstButton.innerHTML = "Pra voltar clica aqui";
+})
+
+thurdButton.addEventListener('click', function onClick(event) {
+    card.style.backgroundColor = 'red';
+    card.innerHTML="<img src='colorado.png'>"
+})
